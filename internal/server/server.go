1971c1fae2f254db081984afde347ecd96cf3aa8
marcus <mawhited@marcuswhited.tech>
2026-07-29 22:06:34 -0400

    feat: starting httpServer

---
+++ internal/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+type Server struct {
+	closed bool
+}
+
+func runConnection(s *Server, conn io.ReadWriteCloser) {
+
+
+	out := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!`")
+	conn.Write(out)
+	conn.Close()
+}
+
+func runServer(s *Server, listener net.Listener) {
+	for {
+		if s.closed {
+			return
+		}
+
+		conn, err := listener.Accept()
+		if err != nil {
+			return
+		}
+		go runConnection(s, conn)
+	}
+}
+
+func Serv(port uint16) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	server := &Server{ closed: false }
+	go runServer(server, listener)
+
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed = true
+	return nil
+}
